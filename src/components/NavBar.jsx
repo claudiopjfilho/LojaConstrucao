ad40c0a0e384103de003535a2bd4ffd6221af78d
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2022-08-18 00:07:04 -0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+export function NavBar() {
+  return (
+    <div className="t-0  w-full h-12 bg-slate-400 flex flex-row gap-2">
+      <h2>Casa Inteligente</h2>
+
+      <h2>Interruptores</h2>
+
+      <h2>Tomadas</h2>
+      <h2>Sensores</h2>
+      <h2>Lâmpadas</h2>
+      <h2>Ver Mais</h2>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/Header";
+import { NavBar } from "../components/NavBar";
+
+export function Home() {
+  return (
+    <div>
+      <NavBar />
+      <div>
+        <Header />
+      </div>
+    </div>
+  );
+}
