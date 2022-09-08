0f46ab19d673f3668a4aab19d317b71004cd6baa
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2022-09-08 17:52:44 -0300

    feat

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,25 @@
 export function About() {
   return (
-    <div>
-      <div>
-        <h2>Categorias</h2>
+    <div className="flex w-full h-36 bg-black text-white p-2">
+      <div className="flex flex-row mx-auto mt-4">
+        <div className="flex flex-row justify-center">
+          <ul>
+            <li>
+              <h2 className="font-poppins text-xs">Categorias</h2>
+            </li>
+            <li>
+              <p>
+                Casa inteligente • Interruptores • Tomadas • Relé • Hubs •
+                Sensores • Lâmpadas • Controles
+              </p>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <h2 className="font-poppins text-xs">Institucional</h2>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
