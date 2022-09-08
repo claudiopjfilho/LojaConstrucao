6417b4dc82274baa62b47df642c9ea2ae21cbe2f
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2022-09-08 19:18:05 -0300

    feat

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,53 @@
 export function About() {
   return (
-    <div className="flex w-full h-36 bg-black text-white p-2">
+    <div className="flex w-full h-36 bg-black text-white p-2 font-poppins">
       <div className="flex flex-row mx-auto mt-4">
-        <div className="flex flex-row justify-center">
-          <ul>
-            <li>
-              <h2 className="font-poppins text-xs">Categorias</h2>
-            </li>
-            <li>
-              <p>
-                Casa inteligente • Interruptores • Tomadas • Relé • Hubs •
-                Sensores • Lâmpadas • Controles
-              </p>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <h2 className="font-poppins text-xs">Institucional</h2>
-            </li>
-          </ul>
+        <div className="flex flex-row  gap-16 ">
+          <div>
+            <h2 className="text-left text-xs">Categorias</h2>
+            <ul>
+              <li className=" mt-4 grid grid-cols-2 gap-1 w-42 items-start text-gray-200">
+                <a href="">• Casa inteligente</a>
+                <a href="">• Interruptores</a>
+                <a href="">• Tomadas</a>
+                <a href="">• Sensores</a>
+                <a href="">• Lâmpadas</a>
+                <a href="">•Controles</a>
+                <a href="">• Relé</a>
+                <a href="">• Hubs</a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="text-left text-xs">Institucional</h2>
+            <ul className="list-inside">
+              <li className=" mt-4     text-gray-200">
+                <a href="">• Fale Conosco</a>
+                <a href="">• Política de Privacidade</a>
+                <a href="">• Quem Somos</a>
+                <a href="">• Troca e Devolução</a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="text-left text-xs">Contato</h2>
+            <ul className="list-inside text-left">
+              <li className=" mt-4     text-gray-200">
+                <a href="">Telefone +55 XX XXXXXXXXX</a>
+                <a href="">Whatsapp +55 XX XXXXXXXXX</a>
+                <a href="">E-mail nome@empresa.com</a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="text-left text-xs">Atendimento</h2>
+            <p className=" mt-4     text-gray-200">
+              Seg à Sáb das 8h00 às 18h00
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -8,7 +8,7 @@ export function Newsletter() {
         <div className="flex flex-row my-auto justify-between">
           <h2 className=" ml-20 font-poppins text-lg">Newsletter</h2>
           <div className=" mb-8 flex flex-row">
-            <p className=" my-auto mr-52 font-poppins ">
+            <p className=" my-auto mr-44 font-poppins ">
               Receba nossas ofertas por e-mail
             </p>
 
@@ -17,9 +17,9 @@ export function Newsletter() {
                 type="email"
                 name="email"
                 placeholder="Digite seu e-mail"
-                className=" w-44 h-8 p-1 my-auto "
+                className=" w-60 h-8 p-1 my-auto text-sm "
               />
-              <img src={Check} alt="" className="bg-black p-2" />
+              <img src={Check} alt="" className="bg-black p-2 cursor-pointer" />
             </div>
           </div>
         </div>
