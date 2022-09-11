95ccd4484a55fedab55e12e0cfea3feec92b4470
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2022-09-11 19:53:45 -0300

    feat

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,11 @@
 import Selo from "../assets/images/site-protegido.png";
 import Logo from "../assets/images/logo-home.png";
+import Elo from "../assets/images/payment/elo.png";
+import Hipercard from "../assets/images/payment/hipercard.png";
+import Mastercard from "../assets/images/payment/mastercard.png";
+import AmericanExpress from "../assets/images/payment/american-express.png";
+import Boleto from "../assets/images/payment/boleto.png";
+import Visa from "../assets/images/payment/visa.png";
 
 export function Footer() {
   return (
@@ -7,6 +13,14 @@ export function Footer() {
       <section className="ml-4 font-poppins text-lg flex">
         <div className=" font-poppins text-md">
           <h2 className="">Pague com:</h2>
+          <div className="grid grid-cols-3 mt-2 gap-2 ">
+            <img src={AmericanExpress} alt="" />
+            <img src={Elo} alt="" />
+            <img src={Visa} alt="" />
+            <img src={Hipercard} alt="" />
+            <img src={Mastercard} alt="" />
+            <img src={Boleto} alt="" />
+          </div>
         </div>
         <div className="mx-auto text-center w-64">
           <div className="flex flex-col">
@@ -17,7 +31,7 @@ export function Footer() {
         </div>
       </section>
       <hr className="mt-8 w-full border-gray-400" />
-      <section className="w-full flex flex-col">
+      <section className="w-full bg-slate-50 flex flex-col">
         <p className="mx-auto mt-4 text-center">
           © Copyright 2022 homemaster – Todos os direitos reservados | Solução
           TI Treinamentos e Produtos de Informática Ltda Me | 10.831.650/0001-32
