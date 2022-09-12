dc5a64205048d3cb4d0e57bb995d39dbfe73c3f3
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2022-09-12 08:25:25 -0300

    feat

---
+++ src/pages/Product.jsx
@@ -0,0 +1,22 @@
+export function Product() {
+  return (
+    <div className="w-full  h-full bg-zinc-50">
+      <div>
+        <div>
+          <Header />
+        </div>
+
+        <div>
+          <News />
+        </div>
+        <div>
+          <Newsletter />
+        </div>
+        <div>
+          <About />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
